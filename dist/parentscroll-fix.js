130e667843674efeccd5bfa2f3193d7ac6daa951
rexblack <mail@benignware.com>
2014-02-04 19:44:55 +0100

    updated build

--- dist/parentscroll-fix.js
+++ dist/parentscroll-fix.js
@@ -7,7 +7,6 @@
     return /iP(hone|od|ad)/.test(navigator.platform);
   })();
   
-  
   var isAndroid = (function() {
     if (/android/i.test(navigator.userAgent)) {
       return true;
@@ -25,6 +24,7 @@
   var doc = document;
   var touchPos = null;
   var overflowContainer = null;
+  var overflowHiddenParent = null;
   
   function isChildOf(child, parent) {
     while((child = child.parentNode) && child !== parent); 
@@ -35,35 +35,33 @@
     return win.getComputedStyle(elem, null).getPropertyValue(styleName);
   }
   
+  function getOverflowHiddenParent(elem) {
+    while(elem && elem.style) if (getStyle(elem, 'overflow') == 'hidden') return elem; else elem = elem.parentNode;
+    return null;
+  }
+  
   function getOverflowContainer(elem) {
     while(elem) {
+      if (elem == doc.body) return null;
       var overflow = getStyle(elem, 'overflow');
-      if (overflow == 'scroll' || overflow == 'auto') {
-        return elem;
-      }
-      elem = elem.parentNode;
-      if (elem == doc) break;
+      if (overflow == 'scroll' || overflow == 'auto') return elem; else elem = elem.parentNode;
     }
     return null;
   }
   
   doc.addEventListener('touchstart', function(e) {
     overflowContainer = getOverflowContainer(e.target);
-    if (overflowContainer) {
-      var touch = event.type == 'touchstart' ? event.changedTouches[0] : event;
-      touchPos = {x: touch.clientX, y: touch.clientY};
-    }
+    overflowHiddenParent = getOverflowHiddenParent(e.target);
+    var touch = event.changedTouches[0];
+    touchPos = { y: touch.clientY};
   });
   
   doc.addEventListener('touchmove', function(e) {
-    overflowContainer = getOverflowContainer(e.target);
-    if (!overflowContainer) return;
-    var touch = event.type == 'touchmove' ? event.changedTouches[0] : event;
-    var touchX = touch.clientX;
+    if (!overflowContainer || !overflowHiddenParent) return;
+    var touch = event.changedTouches[0];
     var touchY = touch.clientY;
-    var dx = (touchX - touchPos.x) * -1;
     var dy = (touchY - touchPos.y) * -1;
-    touchPos = {x: touchX, y: touchY};
+    touchPos = {y: touchY};
     var viewportHeight = overflowContainer.offsetHeight;
     var scrollTop = overflowContainer.scrollTop;
     var scrollHeight = overflowContainer.scrollHeight;
